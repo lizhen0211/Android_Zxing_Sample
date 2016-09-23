cfaeaf485ef9c650eff8f826e8c2b70b49ccdf92
lizhen <lizhen@zhumengyuan.com>
2016-09-23 16:30:25 +0800

    base line

---
+++ app/src/main/java/com/example/lz/android_zxing_sample/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.lz.android_zxing_sample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import widgets.zxing.activity.CaptureActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView resultText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        resultText = (TextView) findViewById(R.id.result_text);
+    }
+
+    public void onStartScannerClick(View view) {
+        Intent openCameraIntent = new Intent(MainActivity.this, CaptureActivity.class);
+        startActivityForResult(openCameraIntent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            if (bundle != null) {
+                String barcode = bundle.getString("result");
+                resultText.setText(barcode);
+            } else {
+                Toast.makeText(MainActivity.this, "扫描失败", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
